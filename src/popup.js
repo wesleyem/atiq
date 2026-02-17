5cb780770311d9237f7026fb3500296c70af5a4b
Wes <wesleyem@users.noreply.github.com>
2026-02-17 10:48:38 -0800

    chore(popup): remove hello world scaffold (#5)

--- src/popup.js
+++ src/popup.js
@@ -1,1 +1,9 @@
-console.log("Hello World extension popup loaded.");
+const openOptionsButton = document.getElementById("open-options");
+
+if (openOptionsButton) {
+  openOptionsButton.addEventListener("click", () => {
+    chrome.runtime.openOptionsPage();
+  });
+}
+
+console.log("AutoTrader Miles Overlay popup loaded.");
